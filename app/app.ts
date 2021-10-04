0e2633ab842e1e61a2fe72c4aaac6cac8c6ec4d1
Ella Bailo <ella.bailo@zoopla.co.uk>
2021-10-04 12:30:25 +0100

    add app

---
+++ app/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import handlebars from 'express-handlebars';
+import { router as userRouter } from './routes/users';
+
+const app = express();
+const port = 4000;
+const error404 = { error: 404, message: 'Page not found.' };
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public'));
+app.set('view engine', 'handlebars');
+
+app.engine(
+  'handlebars',
+  handlebars({
+    layoutsDir: `./views/layouts`,
+    helpers: {
+      math: function (lvalue, operator, rvalue) {
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+        return {
+          '+': lvalue + rvalue,
+        }[operator];
+      },
+      json: function (context) {
+        return JSON.stringify(context);
+      },
+    },
+  })
+);
+
+app.use('/', userRouter);
+app.use((_, res) => {
+  res.render('error-page.handlebars', error404);
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
+
+module.exports = app;
